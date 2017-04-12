7415999d3d521590c35baa205a868068b9e03af5
Desktop <muscaiu@yahoo.com>
2017-04-12 13:03:41 +0300

    add Angular sort method

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -2,36 +2,39 @@
 
 angular.module('mainController', [])
 
-    .controller('mainCtrl', function ($http, $timeout) {
+    .controller('mainCtrl', function ($http) {
 
         var main = this;
 
+        main.orderByField = 'ip';
+        main.reverseSort = false;
+
         main.LocalNetworkArray = [
-            {ip:'192.168.0.1'},
-            {ip:'192.168.0.3'},
-            {ip:'192.168.0.4'},            
-            {ip:'192.168.1.1'},            
-            {ip:'8.8.8.8'}       
+            { ip: '192.168.0.1' },
+            { ip: '192.168.0.3' },
+            { ip: '192.168.0.4' },
+            { ip: '192.168.1.1' },
+            { ip: '8.8.8.8' }
         ]
 
-        function scan() {
+        function Scan() {
             $http.post('api/getData', { test: 'testing data' })
-                .then(function (response) {                                     
+                .then(function (response) {
                     main.pingData = response.data
-                    main.pingData.forEach(function(element){
+                    main.pingData.forEach(function (element) {
                         console.log(element.ip, element.status, element.time)
                     })
 
                     main.LocalNetworkArray.forEach(function (localIp) {
                         main.pingData.forEach(function (serverIp) {
-                            if(localIp.ip == serverIp.ip){
+                            if (localIp.ip == serverIp.ip) {
                                 localIp.status = serverIp.status
                             }
                         })
                     }, this);
                 })
-            setTimeout(scan, 6000);
+            setTimeout(Scan, 6000);
         }
-        scan();
+        Scan();
 
     })
