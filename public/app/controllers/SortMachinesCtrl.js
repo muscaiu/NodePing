661f6c63d3b2fdbd0aa1aa9eea59325ac5e82d3c
Desktop <muscaiu@yahoo.com>
2017-04-21 10:59:19 +0300

    sorting now works with 1 function, more sort options

--- public/app/controllers/SortMachinesCtrl.js
+++ public/app/controllers/SortMachinesCtrl.js
@@ -3,23 +3,10 @@ angular.module('SortMachinesController', [])
     .controller('SortMachinesCtrl', function ($uibModalInstance) {
         var modalInstance = this;
 
-        modalInstance.All = function (option) {
+        modalInstance.Sort = function (option) {
             console.log(option)
             $uibModalInstance.close(option);
         };
-        modalInstance.Triboo = function (option) {
-            console.log(option)
-            $uibModalInstance.close(option);
-        };
-        modalInstance.Server = function (option) {
-            console.log(option)
-            $uibModalInstance.close(option);
-        };
-        modalInstance.Telekom = function (option) {
-            console.log(option)
-            $uibModalInstance.close(option);
-        };
-
         // modalInstance.cancel = function () {
         //     $uibModalInstance.dismiss('cancel');
         // };
