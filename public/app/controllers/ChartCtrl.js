5d96dd5f6d28d52905599ff01d4fe61141d819a8
Desktop <muscaiu@yahoo.com>
2017-04-21 18:00:40 +0300

    hop! si macanache a rezolvat problema :-)

--- public/app/controllers/ChartCtrl.js
+++ public/app/controllers/ChartCtrl.js
@@ -1,11 +1,23 @@
-angular.module('chartController', [])
+angular.module('chartController', ["chart.js"])
 
-    .controller('chartCtrl', function ($uibModalInstance, allMachines) {
-        var chart = this;
-
+    .controller('chartCtrl', function ($scope, $uibModalInstance, allMachines) {
         console.log(allMachines)
-        // modalInstance.cancel = function () {
-        //     $uibModalInstance.dismiss('cancel');
-        // };
+
+        $scope.labels = ['Maran', 'Triboo', 'Telekom', 'Tehnic', 'Web', 'Server', 'Router']
+        $scope.data = [0, 0, 0, 0, 0, 0, 0]
+        $scope.series = ['All Data']
+        // $scope.colors = ["#adabab", "#52aa25"]
+
+        for (let i = 0; i < $scope.labels.length; i++) {
+
+            for (let x = 0; x < allMachines.length; x++) {
+                if ($scope.labels[i] == allMachines[x].Department) {
+                    $scope.data[i]++
+                }
+
+            }
+
+        }
+        console.log($scope.data)
 
     });
